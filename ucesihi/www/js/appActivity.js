5dd1a15f1c1bf3c5219dd2722ad16d06dce83e52
Inas Almanasra <i.almanasra.17@ucl.ac.uk>
2018-04-25 05:47:40 -0400

    mod 2

--- ucesihi/www/js/appActivity.js
+++ ucesihi/www/js/appActivity.js
@@ -1,13 +1,4 @@
-// load a map
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-		
-// load the tiles
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',{
-maxZoom:18,
-attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-id: 'mapbox.streets'
-}).addTo(mymap);
-		
+	
 // create a variable that will hold the XMLHttpRequest() - this must be done outside a function so that all the functions can use the same variable
 var client;
 // and a variable that will hold the layer itself – we need to do this outside the function so that we can use it to remove the layer later on
