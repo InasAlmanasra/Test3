a0ef19758f113fe2ab809a9e35e6f7734be918cc
Inas Almanasra <i.almanasra.17@ucl.ac.uk>
2018-04-25 05:13:51 -0400

    modified code

--- ucesihi/www/js/appActivity.js
+++ ucesihi/www/js/appActivity.js
@@ -1,149 +1,53 @@
-
-// load a map
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-		
-// load the tiles
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',{
-maxZoom:18,
-attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-id: 'mapbox.streets'
-}).addTo(mymap);
-		
-// create a variable that will hold the XMLHttpRequest() - this must be done outside a function so that all the functions can use the same variable
-var client;
-// and a variable that will hold the layer itself – we need to do this outside the function so that we can use it to remove the layer later on
-var POIlayer;
-	
-// create the code to get the POIs data using an XMLHttpRequest
-function getPOI() {
-	client = new XMLHttpRequest();
-
-client.open('GET','http://developer.cege.ucl.ac.uk:30303/getGeoJSON/quiz/geom');
-	client.onreadystatechange = POIResponse; // note don't use POIResponse() withbrackets as that doesn't work
-	client.send();
+function menuClicked() {
+alert("You clicked the menu");
 }
-// create the code to wait for the response from the data server, and process the response once it is received
-function POIResponse() {
-// this function listens out for the server to say that the data is ready - i.e. has state 4
-	if (client.readyState == 4) {
-		// once the data is ready, process the data
-		var POIdata = client.responseText;
-		loadPOIlayer(POIdata);
-	}
-}		
-// convert the received data - which is text - to JSON format and add it to the map
-function loadPOIlayer(POIdata) {
-		
-	// convert the text to JSON
-	var POIjson = JSON.parse(POIdata);
-		
-	// add the JSON layer onto the map -it will apper using the default icons
-	POIlayer = L.geoJson(POIjson).addTo(mymap);
-			
-	//change the map zoom so that all the data is shown
-	mymap.fitBounds(POIlayer.getBounds());
-}
+client.open('GET','http://developer.cege.ucl.ac.uk:30303/getPOI');
 
-		
-//Code to track the user locationremoving the previous markers, now based on https://gis.stackexchange.com/questions/182068/getting-current-user-location-automatically-every-x-seconds-to-put-on-leaflet
-		
-	
-/* // placeholders for the L.marker and L.circle representing user's current position and accuracy    
-var current_position;
-
-function onLocationFound(e) {
-	// if position defined, then remove the existing position marker from the map
-	if (current_position) {
-		mymap.removeLayer(current_position);
-	}
-
-var radius = e.accuracy / 2;
-
-current_position = L.circle(e.latlng,radius).addTo(mymap)
-//.bindPopup("You are within " + radius + " meters from this point").openPopup();
-}
-	
-function onLocationError(e) {
-	alert(e.message);
-}
-
-mymap.on('locationfound', onLocationFound);
-mymap.on('locationerror', onLocationError);
-
-// wrap map.locate in a function    
-function locate() {
-	mymap.locate({setView: true, maxZoom: 16});
-}
-
-// call locate every 3 seconds... forever
-setInterval(locate, 3000);
-*/
-	
-// Second alternative
-	
-// code to track the user location
-var position_marker
+// The track location code building
+var locationFinder
 			
 function trackLocation() {
 	if (navigator.geolocation) {
-		navigator.geolocation.watchPosition(showPosition);
-		navigator.geolocation.getCurrentPosition(getDistanceFromPoint);
+		navigator.geolocation.watchlocation(locationShower);
+		navigator.geolocation.getCurrentlocation(distanceBetweenPoints);
 		} else {
 			document.getElementById('showLocation').innerHTML = "Geolocation is not supported by this browser.";
 		}
 }
 		
-function showPosition(position) {
-	if (position_marker){
-		mymap.removeLayer(position_marker);
+function locationShower(location) {
+	if (locationFinder){
+		mymap.removeLayer(locationFinder);
 	}
-	position_marker = L.circleMarker([position.coords.latitude, position.coords.longitude], {radius: 4}).addTo(mymap);
-	mymap.setView([position.coords.latitude, position.coords.longitude], 25);
+	locationFinder = L.circleMarker([location.coords.latitude, location.coords.longitude], {radius: 4}).addTo(mymap);
+	mymap.setView([location.coords.latitude, location.coords.longitude], 25);
 }
 		
-// get distance
-	
-/* (Now this code is inside trackLocation) 
-function getDistance(){
-	alert('getting distance')
-	// getDistanceFromPoint is the function called once the distance has been found
-	navigator.geolocation.getCurrentPosition(getDistanceFromPoint);	
-} */
 
-function getDistanceFromPoint(position){
+
+function distanceBetweenPoints(location){
 	//find the coordinates of a point to test using this website: https://itouchmap.com/latlong.html
-	// these are the coordinates of my building's garden
 	var lat = 51.557102 
 	var lng = -0.113329
 	// returns the distance in kilometers
-	var distance = calculateDistance(position.coords.latitude, position.coords.longitude, lat,lng, 'K');
+	var distance = distanceCalculater(location.coords.latitude, location.coords.longitude, lat,lng, 'K');
 	document.getElementById('showDistance').innerHTML = "Distance: " + distance;
 	
-	var alertRadius = 0.06
-	/* // code to create a proximity alert 1er intento
-		if (distance < 0.06) {
-			position_marker.bindPopup("</b>la distancia es menor a 0.06<br/>and alternatives.");
-		} else {
-			position_marker.bindPopup("</b>la distancia es mayor a 0.06<br/>and alternatives.");
-		} */
+	var defineRadius = 0.05
+
 	
-// code to create a proximity alert 2do intento
-	if (distance < alertRadius) {
-		alert("you are close to a point of interest!!!!");
-		/* var popup = L.popup()
-		.setLatLng(51.557102 -0.113329)
-		.setContent('<p>menor!<br />posible respuesta 1.</p>')
-		.openOn(mymap); */
+// The following if-statement code alrets the user of near quiz points; or if the user is far from the points:
+	if (distance < defineRadius) {
+		alert("Alert, you are near a quiz point");
+		
 	} else { 
-		alert("You are not close yet to a point of interest!!!!");
-		/* L.popup()
-		.setLatLng(51.557102 -0.113329)
-		.setContent('<p>mayor!<br />posible respuesta 1.</p>')
-		.openOn(mymap); */
+		alert("You are far from our game");
+		
 	}	
 }
+// This code calculates the distance of the user to the points of interest:
 // code adapted from https://www.htmlgoodies.com/beyond/javascript/calculate-the-distance-between-two-points-inyour-web-apps.html
-function calculateDistance(lat1, lon1, lat2, lon2, unit) {
+function distanceCalculater(lat1, lon1, lat2, lon2, unit) {
 	var radlat1 = Math.PI * lat1/180;
 	var radlat2 = Math.PI * lat2/180;
 	var radlon1 = Math.PI * lon1/180;
@@ -152,19 +56,14 @@ function calculateDistance(lat1, lon1, lat2, lon2, unit) {
 	var radtheta = Math.PI * theta/180;
 	var subAngle = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
 	subAngle = Math.acos(subAngle);
-	subAngle = subAngle * 180/Math.PI; // convert the degree value returned by acos back to degrees from radians
-	dist = (subAngle/360) * 2 * Math.PI * 3956; // ((subtended angle in degrees)/360) * 2 * pi * radius )
+	subAngle = subAngle * 180/Math.PI; // convert from radians to degrees
+	dist = (subAngle/360) * 2 * Math.PI * 3956; // Mathetimatical Equation to calculate the distance
                                              // where radius of the earth is 3956 miles
-	if (unit=="K") { dist = dist * 1.609344 ;}  // convert miles to km
-	if (unit=="N") { dist = dist * 0.8684 ;}    // convert miles to nautical miles
+	if (unit=="K") { dist = dist * 1.609344 ;}  // This code converts miles to km
+	if (unit=="N") { dist = dist * 0.8684 ;}    // This code converts miles to nautical miles
 	return dist;
 }
 
-
-	
-
-	
-	//////////////
 	
 	var xhr; // define the global variable to process the AJAX request
 	function callDivChange() {
